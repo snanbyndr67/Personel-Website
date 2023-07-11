c32e3d2cc1e918209bb6e86eb754a9cc193dcff5
snanbyndr67 <snan677@yandex.com>
2023-07-11 17:03:12 +0300

    revize

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 const navbarToggle = document.querySelector('.navbar-toggle');
-const navbarMenu = document.querySelector('.navbar-menu');
+const navbarMenu = document.querySelector('.navbar');
 
 navbarToggle.addEventListener('click', function() {
   navbarMenu.classList.toggle('active');
